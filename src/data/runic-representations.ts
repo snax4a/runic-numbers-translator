551037e21a14d2b092d6f70d2e013c689325bf01
Szymon Sus <sus.szymon3@gmail.com>
2022-11-18 15:45:25 +0100

    feat: add all runic representations

--- src/data/runic-representations.ts
+++ src/data/runic-representations.ts
@@ -4,10 +4,329 @@ export const runicRepresentations: RunicRepresentation[] = [
   {
     decimalValue: 1,
     pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'h', parameter: '28' },
+    ],
+  },
+  {
+    decimalValue: 2,
+    pathCommands: [
       { letter: 'm', parameter: '0,0' },
-      { letter: 'h', parameter: '30' },
-      { letter: 'h', parameter: '-30' },
       { letter: 'v', parameter: '100' },
+      { letter: 'v', parameter: '-70' },
+      { letter: 'h', parameter: '28' },
+    ],
+  },
+  {
+    decimalValue: 3,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'l', parameter: '28,30' },
+    ],
+  },
+  {
+    decimalValue: 4,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'v', parameter: '30' },
+      { letter: 'l', parameter: '28,-30' },
+    ],
+  },
+  {
+    decimalValue: 5,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'v', parameter: '30' },
+      { letter: 'l', parameter: '28,-30' },
+      { letter: 'h', parameter: '-28' },
+    ],
+  },
+  {
+    decimalValue: 6,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'm', parameter: '28, 0' },
+      { letter: 'v', parameter: '30' },
+    ],
+  },
+  {
+    decimalValue: 7,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'h', parameter: '28' },
+      { letter: 'v', parameter: '30' },
+    ],
+  },
+  {
+    decimalValue: 8,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'm', parameter: '28, 0' },
+      { letter: 'v', parameter: '30' },
+      { letter: 'h', parameter: '-28' },
+    ],
+  },
+  {
+    decimalValue: 9,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'h', parameter: '28' },
+      { letter: 'v', parameter: '30' },
+      { letter: 'h', parameter: '-28' },
+    ],
+  },
+  {
+    decimalValue: 10,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'h', parameter: '-28' },
+    ],
+  },
+  {
+    decimalValue: 20,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'v', parameter: '30' },
+      { letter: 'h', parameter: '-28' },
+    ],
+  },
+  {
+    decimalValue: 30,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'l', parameter: '-28,30' },
+    ],
+  },
+  {
+    decimalValue: 40,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'v', parameter: '30' },
+      { letter: 'l', parameter: '-28,-30' },
+    ],
+  },
+  {
+    decimalValue: 50,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'v', parameter: '30' },
+      { letter: 'l', parameter: '-28,-30' },
+      { letter: 'h', parameter: '28' },
+    ],
+  },
+  {
+    decimalValue: 60,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'm', parameter: '-28, 0' },
+      { letter: 'v', parameter: '30' },
+    ],
+  },
+  {
+    decimalValue: 70,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'h', parameter: '-28' },
+      { letter: 'v', parameter: '30' },
+    ],
+  },
+  {
+    decimalValue: 80,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'm', parameter: '-28, 0' },
+      { letter: 'v', parameter: '30' },
+      { letter: 'h', parameter: '28' },
+    ],
+  },
+  {
+    decimalValue: 90,
+    pathCommands: [
+      { letter: 'm', parameter: '0,100' },
+      { letter: 'v', parameter: '-100' },
+      { letter: 'h', parameter: '-28' },
+      { letter: 'v', parameter: '30' },
+      { letter: 'h', parameter: '28' },
+    ],
+  },
+  {
+    decimalValue: 100,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'h', parameter: '28' },
+    ],
+  },
+  {
+    decimalValue: 200,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '100' },
+      { letter: 'v', parameter: '-30' },
+      { letter: 'h', parameter: '28' },
+    ],
+  },
+  {
+    decimalValue: 300,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'l', parameter: '28,-30' },
+    ],
+  },
+  {
+    decimalValue: 400,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'v', parameter: '-30' },
+      { letter: 'l', parameter: '28,30' },
+    ],
+  },
+  {
+    decimalValue: 500,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'v', parameter: '-30' },
+      { letter: 'l', parameter: '28,30' },
+      { letter: 'h', parameter: '-28' },
+    ],
+  },
+  {
+    decimalValue: 600,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'm', parameter: '28, 0' },
+      { letter: 'v', parameter: '-30' },
+    ],
+  },
+  {
+    decimalValue: 700,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'h', parameter: '28' },
+      { letter: 'v', parameter: '-30' },
+    ],
+  },
+  {
+    decimalValue: 800,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'm', parameter: '28, 0' },
+      { letter: 'v', parameter: '-30' },
+      { letter: 'h', parameter: '-28' },
+    ],
+  },
+  {
+    decimalValue: 900,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'h', parameter: '28' },
+      { letter: 'v', parameter: '-30' },
+      { letter: 'h', parameter: '-28' },
+    ],
+  },
+  {
+    decimalValue: 1000,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'h', parameter: '-28' },
+    ],
+  },
+  {
+    decimalValue: 2000,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '100' },
+      { letter: 'v', parameter: '-30' },
+      { letter: 'h', parameter: '-28' },
+    ],
+  },
+  {
+    decimalValue: 3000,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'l', parameter: '-28,-30' },
+    ],
+  },
+  {
+    decimalValue: 4000,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'v', parameter: '-30' },
+      { letter: 'l', parameter: '-28,30' },
+    ],
+  },
+  {
+    decimalValue: 5000,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'v', parameter: '-30' },
+      { letter: 'l', parameter: '-28,30' },
+      { letter: 'h', parameter: '28' },
+    ],
+  },
+  {
+    decimalValue: 6000,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'm', parameter: '-28, 0' },
+      { letter: 'v', parameter: '-30' },
+    ],
+  },
+  {
+    decimalValue: 7000,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'h', parameter: '-28' },
+      { letter: 'v', parameter: '-30' },
+    ],
+  },
+  {
+    decimalValue: 8000,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'm', parameter: '-28, 0' },
+      { letter: 'v', parameter: '-30' },
+      { letter: 'h', parameter: '28' },
+    ],
+  },
+  {
+    decimalValue: 9000,
+    pathCommands: [
+      { letter: 'm', parameter: '0,0' },
+      { letter: 'v', parameter: '98' },
+      { letter: 'h', parameter: '-28' },
+      { letter: 'v', parameter: '-30' },
+      { letter: 'h', parameter: '28' },
     ],
   },
 ];
